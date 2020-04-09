e76c6fcd3e66cb33cf475a8dd8014ac976c66044
line2nine <hailinh203@gmail.com>
2020-04-09 13:38:42 +0700

    Add files

---
+++ Application.php
@@ -0,0 +1,20 @@
+<?php
+
+
+class Application
+{
+    private static $instance;
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance()
+    {
+        if (self::$instance === null) {
+            self::$instance = new Application();
+            echo 'alo';
+        }
+        return self::$instance;
+    }
+}
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include "Application.php";
+
+$app1 = Application::getInstance();
+$app2 = Application::getInstance();
+//$app3 = new Application(); //Error
